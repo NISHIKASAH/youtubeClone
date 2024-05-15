7ac9bc9c6953ee9719817291be687e898ebdccb5
NISHIKASAH <81612543+NISHIKASAH@users.noreply.github.com>
2024-05-16 00:26:13 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-// const GOOGLE_API_KEY = "AIzaSyAem1jpD5GIZ6lj_G2FmpI24J7cO2bQn54";
+
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
